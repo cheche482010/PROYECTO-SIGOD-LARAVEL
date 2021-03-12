bf4604fc8fc89b69e8d9955a67663aabd79e17d0
bracho <brachojd22@gmail.com>
2021-03-11 21:32:20 -0400

    crud vy vistas actividades
    jesus bracho

--- LARAVEL-SIGOD/database/seeds/DatabaseSeeder.php
+++ LARAVEL-SIGOD/database/seeds/DatabaseSeeder.php
@@ -1,42 +1,16 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Seed the application's database.
      *
      * @return void
      */
-    public function run() 
+    public function run()
     {
-        $this->call(ActividadesSeeder::class);
-
-        $this->call(AreasSeeder::class);
-
-        $this->call(AulasSeeder::class);
-
-        $this->call(CategoriasSeeder::class);
-
-        $this->call(ConcursosSeeder::class);
-
-        $this->call(DedicacionesSeeder::class);
-
-        $this->call(DependenciasSeeder::class);
-
-        $this->call(DocentesSeeder::class);
-
-        $this->call(Ejes_FormacionSeeder::class);
-
-        $this->call(IngresosSeeder::class);
-
-        $this->call(MunicipiosSeeder::class);
-
-        $this->call(Relaciones_LaboralesSeeder::class);
-
-        $this->call(Tipos_ActividadesSeeder::class);
-
-        $this->call(Unidades_CurricularesSeeder::class);
+        // $this->call(UsersTableSeeder::class);
     }
 }
